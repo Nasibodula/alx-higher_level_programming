27c5f4f75d6a28b3cae61352343afc0a602834cc
Nasibodula <nasiochudo@gmail.com>
2023-07-04 22:03:45 +0300

    first attempt

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """Define a class Square."""
+
 class Square:
+
     def __init__(self, size):
         """Initialize a new square.
         Args:
             size (int): The size of the new square.
         """
         self.size = size
+
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    @size.setter
+   @size.setter
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
