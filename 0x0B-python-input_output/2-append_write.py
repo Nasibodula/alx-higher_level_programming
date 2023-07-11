b7ea6cb2089d6fefd0cc6911456198f21a2681ab
Nasibodula <nasiochudo@gmail.com>
2023-07-11 22:59:37 +0300

    first updates

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a file-appending function."""
+
+def append_write(filename="", text=""):
+    """Appends a string to the end of  UTF8 text file.
+    Returns:
+        The number of characters appended.
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
